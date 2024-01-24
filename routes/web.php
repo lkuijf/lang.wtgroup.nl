6034472de77cfee6f5863aab9b1f6dc0ddb00c5f
Leon Kuijf <leon.kuijf@gmail.com>
2024-01-24 09:24:27 +0100

    added controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+
 /**
  * Application routes.
  */
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::any('/', [PageController::class, 'front']);
+Route::any('page', [PageController::class, 'page']);
+Route::any('search', [PageController::class, 'search']);
+
+Route::any('single', [PostController::class, 'single']);
+Route::any('archive', [PostController::class, 'collection']);
+Route::any('category', [PostController::class, 'collection']);
+Route::any('tag', [PostController::class, 'collection']);
+Route::any('blog', [PostController::class, 'collection']);
+
+Route::fallback([PageController::class, 'error404']);
